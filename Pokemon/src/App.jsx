0116bca29ec45b1b21d0ff02f27f3249853c600a
anikaivnath <anikaivnath8@gmail.com>
2025-01-03 23:15:35 +0600

    add favorite icon

--- Pokemon/src/App.jsx
+++ Pokemon/src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHeart, faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
 const App = () => {
   return (
     <div>
       <div className="sticky top-0 z-10 bg-amber-200 p-4 flex items-center justify-between shadow-md">
         <p className="font-serif text-3xl">Explore Your Favorite Pokémon Card</p>
-        <div className='flex gap-6'>
-        <input
-          type="text"
-          placeholder="Search Pokémon..."
-          className="border border-gray-400 rounded-md px-3 py-1 focus:outline-none focus:ring-2 focus:ring-amber-400"
-        />
-         <p><FontAwesomeIcon icon={faHeart} /></p>
+        
+        <div className="flex gap-6 items-center">
+          <p><FontAwesomeIcon icon={faHeart} className="text-3xl text-red-600 cursor-pointer" /></p>
+          <input
+            type="text"
+            placeholder="Search Pokémon..."
+            className="border border-gray-400 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-amber-400 w-64"
+          />
         </div>
-       
-
-      
-  
-
       </div>
     </div>
   );
