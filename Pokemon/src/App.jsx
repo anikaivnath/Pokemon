127c59972655a7a9a6ce13b8b93fda7753051fc1
anikaivnath <anikaivnath8@gmail.com>
2025-01-03 20:00:18 +0600

    Add heading

---
+++ Pokemon/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+    <div className=' text-center bg'>
+      <p>Explore your Favorite Pokemon Card</p>
+    </div>
+
+    </div>
+  )
+}
+
+export default App
