17187a601df58bc3472530582c8f20364b0bc9d2
anikaivnath <anikaivnath8@gmail.com>
2025-01-07 22:15:27 +0600

    Refine styling in Details component for better layout and readability

--- Pokemon/src/Details.jsx
+++ Pokemon/src/Details.jsx
@@ -14,7 +14,7 @@ const Details = () => {
   if (!pokemon) return <div>Loading...</div>;
 
   return (
-    <div className="w-80">
+    <div className="w-80 text-center m-auto mt-8">
    
       <div className="flex flex-row justify-center ">
         <img
@@ -28,12 +28,12 @@ const Details = () => {
       <div>
       <h1 className="text-3xl font-bold mb-4 text-center ">{pokemon.name.toUpperCase()}</h1>
       </div>
-      <div className="flex flex-row justify-center gap-6"> 
+      <div className=""> 
 
-      <div className="mb-4 ">
+      <div className="mb-4 flex flex-row  ">
       
-        <h2 className="text-2xl font-semibold">Abilities:</h2>
-        <ul>
+        <h2 className="text-2xl font-semibold ">Abilities:</h2>
+        <ul className="ml-4 mt-2">
           {pokemon.abilities.map((ability) => (
             <li key={ability.ability.name} className="capitalize">{ability.ability.name}</li>
           ))}
