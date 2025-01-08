a10f202413f69e09c20f9ddcd619bbe81ad79b16
anikaivnath <anikaivnath8@gmail.com>
2025-01-08 13:59:36 +0600

    Update Favorites component styling for improved visual appeal

--- Pokemon/src/Favorite.jsx
+++ Pokemon/src/Favorite.jsx
@@ -17,14 +17,14 @@ const Favorites = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Your Favorite Pokémon</h1>
+      <h1 className="text-2xl font-bold mb-4 ">Your Favorite Pokémon</h1>
 
       {favorites.length === 0 ? (
         <p>No Pokémon added to favorites yet!</p>
       ) : (
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {favorites.map((pokemonName, index) => (
-            <div key={index} className="p-4 border rounded-lg hover:bg-gray-200 transition-all">
+            <div key={index} className="p-4 border bg-green-800 rounded-lg hover:bg-green-700 transition-all">
               <img
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
                 alt={pokemonName}
